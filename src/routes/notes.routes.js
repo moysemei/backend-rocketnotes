6fe479fd08d3532288838a60353599cb99544562
moyse <moysemei@hotmail.com>
2024-03-06 21:27:10 -0300

    second commit

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+
+const NotesController = require("../controllers/NotesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const notesRouter = Router();
+
+const notesController = new NotesController();
+
+notesRouter.use(ensureAuthenticated);
+
+notesRouter.post("/", notesController.create);
+notesRouter.get("/:id", notesController.show);
+notesRouter.delete("/:id", notesController.delete);
+notesRouter.get("/", notesController.index);
+
+module.exports = notesRouter;
